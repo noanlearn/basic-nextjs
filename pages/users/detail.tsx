cf3d2bc56961d47c136fdc07b4964d25f8b254b8
noanlearn <noviadryn@gmail.com>
2023-01-13 16:42:14 +0700

    🎨 adding Layout for Pages

---
+++ pages/users/detail.tsx
@@ -0,0 +1,5 @@
+export default function UserDetail() {
+  return (
+    <div>UserDetail</div>
+  )
+}
---
+++ pages/users/index.tsx
@@ -0,0 +1,9 @@
+import Layout from '@/components/Layout'
+
+export default function Users() {
+  return (
+    <Layout>
+      <p>Users Page</p>
+    </Layout>
+  )
+}
