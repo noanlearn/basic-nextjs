40e4b756b411c205c0d1649d5189404875b8843d
noanlearn <noviadryn@gmail.com>
2023-01-13 16:49:35 +0700

    custome 404 page

---
+++ pages/404.tsx
@@ -0,0 +1,8 @@
+export default function Custom404() {
+  return (
+    <div>
+      <h1 className="title-not-found">Ooopss...</h1>
+      <h1 className="title-not-found">Halaman yang anda cari tidak ditemukan</h1>
+    </div>
+  )
+}
